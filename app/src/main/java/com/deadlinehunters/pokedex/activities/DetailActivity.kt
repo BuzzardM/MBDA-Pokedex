8251e170001dda7060ccfc333f02e553e2e1f059
BuzzardM <mfbuijse@avans.nl>
2021-04-04 00:42:30 +0200

    added pokemon details parser

--- app/src/main/java/com/deadlinehunters/pokedex/activities/DetailActivity.kt
+++ app/src/main/java/com/deadlinehunters/pokedex/activities/DetailActivity.kt
@@ -1,17 +1,20 @@
 package com.deadlinehunters.pokedex.activities
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import android.util.AttributeSet
+import android.util.Log
+import android.view.View
 import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import com.deadlinehunters.pokedex.R
-import com.deadlinehunters.pokedex.adapters.PokemonResultAdapter
 import com.deadlinehunters.pokedex.model.Pokemon
 import com.deadlinehunters.pokedex.model.PokemonResult
+import com.deadlinehunters.pokedex.model.PokemonStat
+import com.deadlinehunters.pokedex.model.PokemonType
 
 class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,27 +22,51 @@ class DetailActivity : AppCompatActivity() {
         setContentView(R.layout.activity_detail)
     }
 
+    override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
+        val requestQueue = Volley.newRequestQueue(applicationContext)
+        getPokemonResults(requestQueue)
+        return super.onCreateView(name, context, attrs)
+    }
+
     private fun getPokemonResults(requestQueue: RequestQueue) {
         val pokemonData: PokemonResult? = intent.getParcelableExtra("pokemon_data")
         val url = pokemonData?.url
-        val pokemonResults = mutableListOf<Pokemon>()
+        val pokemon = mutableListOf<Pokemon>()
 
         val request = JsonObjectRequest(
             Request.Method.GET, url, null,
             { response ->
-                val results = response.getJSONArray("results")
-                
-                for (i in 0 until results.length()) {
-                    val result = results.getJSONObject(i)
-                    pokemonResults.add(
-                        Pokemon(
-                            result.getString("id").toInt(),
-                            result.getString("name"),
-                            result.getString("height").toInt(),
+                val height = response.getInt("height")
+                val weight = response.getInt("weight")
+                val id = response.getInt("id")
+                val name = response.getString("name")
+                val jsonStats = response.getJSONArray("stats")
+                val jsonTypes = response.getJSONArray("types")
 
+                val stats = mutableListOf<PokemonStat>()
+                val types = mutableListOf<PokemonType>()
+
+                for (i in 0 until jsonStats.length()) {
+                    val result = jsonStats.getJSONObject(i)
+                    stats.add(
+                        PokemonStat(
+                            result.getInt("base_stat"),
+                            result.getJSONObject("stat").getString("name")
                         )
                     )
                 }
+
+                for (i in 0 until jsonTypes.length()) {
+                    val result = jsonTypes.getJSONObject(i)
+                    types.add(
+                        PokemonType(
+                            result.getInt("slot"),
+                            result.getJSONObject("type").getString("name")
+                        )
+                    )
+                }
+
+                pokemon.add(Pokemon(id, name, height, weight, stats, types))
             },
             { error ->
                 error.printStackTrace()
--- app/src/main/java/com/deadlinehunters/pokedex/model/DataClasses.kt
+++ app/src/main/java/com/deadlinehunters/pokedex/model/DataClasses.kt
@@ -6,8 +6,8 @@ import kotlinx.android.parcel.Parcelize
 @Parcelize
 data class PokemonResult (val name: String, val url: String) : Parcelable
 
-data class Pokemon (val id: Int, val name: String, val height: Int, val weight: Int, val stats: List<PokemonStats>, val types: List<PokemonTypes>);
+data class Pokemon (val id: Int, val name: String, val height: Int, val weight: Int, val stats: List<PokemonStat>, val types: List<PokemonType>)
 
-data class PokemonStats (val value: Int, val name: String);
+data class PokemonStat (val value: Int, val name: String)
 
-data class PokemonTypes (val slot: Int, val typeName: String);
+data class PokemonType (val slot: Int, val typeName: String)
